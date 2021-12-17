fe583540d7494470064940e8865c81052772a00f
orcunerdil <orcun_erdil@hotmail.com>
2021-12-17 19:08:01 +0300

    Ornek Template Yuklendı

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -1,40 +1,50 @@
-<!DOCTYPE html>
-<html lang="en">
 <head>
-    <title>@yield('title')</title>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+    <title>@yield('title')</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="@yield('title')">
+    <meta name="keywords" content="@yield('keywords')">
+    <meta name="author" content="Orcun Erdil">
+
+
+    <!--[if ie]><meta content='IE=8' http-equiv='X-UA-Compatible'/><![endif]-->
+    <!-- bootstrap -->
+    <link href="{{asset('assets')}}/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="{{asset('assets')}}/bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet">
+
+    <link href="{{asset('assets')}}/themes/css/bootstrappage.css" rel="stylesheet"/>
+
+    <!-- global styles -->
+    <link href="{{asset('assets')}}/themes/css/flexslider.css" rel="stylesheet"/>
+    <link href="{{asset('assets')}}/themes/css/main.css" rel="stylesheet"/>
+
+    <!-- scripts -->
+    <script src="{{asset('assets')}}/themes/js/jquery-1.7.2.min.js"></script>
+    <script src="{{asset('assets')}}/bootstrap/js/bootstrap.min.js"></script>
+    <script src="{{asset('assets')}}/themes/js/superfish.js"></script>
+    <script src="{{asset('assets')}}/themes/js/jquery.scrolltotop.js"></script>
+    <!--[if lt IE 9]>
+    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <script src="{{asset('assets')}}/js/respond.min.js"></script>
+    <![endif]-->
     @yield('css')
-    @yield('js')
-
-
+    @yield('tophs')
+    @yield('headerjs')
 </head>
 <body>
 
-<div class="jumbotron text-center">
-    <h1>Header</h1>
-    <p>Resize this responsive page to see the effect!</p>
-    <a href="/">Home</a>
-    <a href="/aboutus">About Us</a>
-    @yield('header')
-</div>
+@include('home._header')
+@include('home._slider')
 
-<div class="container">
-    <div class="row">
-     @yield('sidebar')
-        @yield('content')
-    </div>
-</div>
+@section('content')
 
-<div class="jumbotron text-center">
-    <h1>Footer</h1>
-    <p>Sayfa Altı</p>
-    @yield('footer')
-</div>
+
+@show
+
+
+@include('home._footer')
+@yield('footerjs')
 
 </body>
 </html>
